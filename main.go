408db18c1f3db7eeb0db7a5908ea4390811aeb1f
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2024-01-18 13:11:08 +0100

    ajout de la func pour choisir un joueur

--- main.go
+++ main.go
@@ -13,5 +13,25 @@ var joueurActuel string
 var gagnant string
 var finJeu = false
 
+func choixJoueur() {
+	fmt.Print("Veuillez choisir soit une croix (X), soit un rond (O) : ")
+	fmt.Scan(&joueurActuel)
+
+	for {
+		joueurActuel = strings.ToUpper(joueurActuel)
+		if joueurActuel == "X" {
+			fmt.Println("Vous avez choisi X. Le joueur 2 aura O")
+			break
+		} else if joueurActuel == "O" {
+			fmt.Println("Vous avez choisi O. Le joueur 2 aura X")
+			break
+		} else {
+			fmt.Print("Veuillez choisir soit (X) soit (O) : ")
+			fmt.Scan(&joueurActuel)
+		}
+	}
+}
+
+
 
 
