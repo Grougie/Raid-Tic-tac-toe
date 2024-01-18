c65d52d2d5e9d2249e5de4a7b161fc102a291c8a
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2024-01-18 14:15:48 +0100

    variable en anglais

--- jeu.go
+++ jeu.go
@@ -5,45 +5,45 @@ import (
 	"strings"
 )
 
-var grille = []string{"-", "-", "-",
+var grid = []string{"-", "-", "-",
 	"-", "-", "-",
 	"-", "-", "-"}
 
-var joueurActuel string
-var gagnant string
-var finJeu = false
+var currentPlayer string
+var winner string
+var endGame = false
 
 func choicePlayer() {
 	fmt.Print("Please choose either a cross (X) or a circle (O) : ")
-	fmt.Scan(&joueurActuel)
+	fmt.Scan(&currentPlayer)
 
 	for {
-		joueurActuel = strings.ToUpper(joueurActuel)
-		if joueurActuel == "X" {
+		currentPlayer = strings.ToUpper(currentPlayer)
+		if currentPlayer == "X" {
 			fmt.Println("You chose X. Player 2 will have O")
 			break
-		} else if joueurActuel == "O" {
+		} else if currentPlayer == "O" {
 			fmt.Println("You chose O. Player 2 will have X")
 			break
 		} else {
 			fmt.Print("Please choose either (X) or (O) : ")
-			fmt.Scan(&joueurActuel)
+			fmt.Scan(&currentPlayer)
 		}
 	}
 }
 
 func printGrid() {
 	fmt.Println("\n-------------")
-	fmt.Printf("| %s | %s | %s |      | 1 | 2 | 3 |\n", grille[0], grille[1], grille[2])
+	fmt.Printf("| %s | %s | %s |      | 1 | 2 | 3 |\n", grid[0], grid[1], grid[2])
 	fmt.Println("-------------")
-	fmt.Printf("| %s | %s | %s |      | 4 | 5 | 6 |\n", grille[3], grille[4], grille[5])
+	fmt.Printf("| %s | %s | %s |      | 4 | 5 | 6 |\n", grid[3], grid[4], grid[5])
 	fmt.Println("-------------")
-	fmt.Printf("| %s | %s | %s |      | 7 | 8 | 9 |\n", grille[6], grille[7], grille[8])
+	fmt.Printf("| %s | %s | %s |      | 7 | 8 | 9 |\n", grid[6], grid[7], grid[8])
 	fmt.Printf("\n")
 }
 
-func round(joueur string) {
-	fmt.Printf("It's the player's turn: %s\n", joueur)
+func round(player string) {
+	fmt.Printf("It's the player's turn: %s\n", player)
 	var pos string
 	fmt.Print("Please select an empty space on the grid between 1 and 9 : ")
 	fmt.Scan(&pos)
@@ -56,7 +56,7 @@ func round(joueur string) {
 			}
 		}
 		index := parsePosition(pos)
-		if index >= 0 && index < 9 && grille[index] == "-" {
+		if index >= 0 && index < 9 && grid[index] == "-" {
 			valide = true
 		} else {
 			fmt.Print("Error, You cannot access this position. Please select an empty space on the grid between 1 and 9 : ")
@@ -65,7 +65,7 @@ func round(joueur string) {
 	}
 
 	index := parsePosition(pos)
-	grille[index] = joueur
+	grid[index] = player
 	printGrid()
 }
 
@@ -81,36 +81,36 @@ func endGameChecker() {
 }
 
 func victory() {
-	if grille[0] == grille[1] && grille[1] == grille[2] && grille[2] != "-" ||
-		grille[3] == grille[4] && grille[4] == grille[5] && grille[5] != "-" ||
-		grille[6] == grille[7] && grille[7] == grille[8] && grille[8] != "-" ||
-		grille[0] == grille[3] && grille[3] == grille[6] && grille[6] != "-" ||
-		grille[1] == grille[4] && grille[4] == grille[7] && grille[7] != "-" ||
-		grille[2] == grille[5] && grille[5] == grille[8] && grille[8] != "-" ||
-		grille[0] == grille[4] && grille[4] == grille[8] && grille[8] != "-" ||
-		grille[2] == grille[4] && grille[4] == grille[6] && grille[6] != "-" {
-		finJeu = true
-		gagnant = grille[0]
+	if grid[0] == grid[1] && grid[1] == grid[2] && grid[2] != "-" ||
+		grid[3] == grid[4] && grid[4] == grid[5] && grid[5] != "-" ||
+		grid[6] == grid[7] && grid[7] == grid[8] && grid[8] != "-" ||
+		grid[0] == grid[3] && grid[3] == grid[6] && grid[6] != "-" ||
+		grid[1] == grid[4] && grid[4] == grid[7] && grid[7] != "-" ||
+		grid[2] == grid[5] && grid[5] == grid[8] && grid[8] != "-" ||
+		grid[0] == grid[4] && grid[4] == grid[8] && grid[8] != "-" ||
+		grid[2] == grid[4] && grid[4] == grid[6] && grid[6] != "-" {
+		endGame = true
+		winner = grid[0]
 	}
 }
 
 func egalityGame() {
-	if !strings.Contains(strings.Join(grille, ""), "-") {
-		finJeu = true
+	if !strings.Contains(strings.Join(grid, ""), "-") {
+		endGame = true
 	}
 }
 
 func nextPlayer() {
-	if joueurActuel == "X" {
-		joueurActuel = "O"
+	if currentPlayer == "X" {
+		currentPlayer = "O"
 	} else {
-		joueurActuel = "X"
+		currentPlayer = "X"
 	}
 }
 
 func result() {
-	if gagnant == "X" || gagnant == "O" {
-		fmt.Printf("The player : %s won!!\n", gagnant)
+	if winner == "X" || winner == "O" {
+		fmt.Printf("The player : %s won!!\n", winner)
 	} else {
 		fmt.Println("Egality game")
 	}
@@ -119,8 +119,8 @@ func result() {
 func game() {
 	choicePlayer()
 	printGrid()
-	for !finJeu {
-		round(joueurActuel)
+	for !endGame {
+		round(currentPlayer)
 		endGameChecker()
 		nextPlayer()
 	}
